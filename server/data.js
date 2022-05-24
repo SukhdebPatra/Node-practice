54ab60bb58b53fd3c0557412a4806452095841bb
SukhdebPatra <sukhdebpatra1@gmail.com>
2022-05-24 17:02:09 +0530

    Http server and data sent

---
+++ server/data.js
@@ -0,0 +1,11 @@
+const data=[{ name: 'Sukhdeb', email: 'sukhdeb@text.com' },
+{ name: 'Sukhdeb', email: 'sukhdeb@text.com' },
+{ name: 'Bidhut', email: 'Bidhut@text.com' },
+{ name: 'Beena', email: 'Beena@text.com' },
+{ name: 'Subham', email: 'subham@text.com' },
+{ name: 'Shekhar', email: 'Shekhar@text.com' },
+{ name: 'ronit', email: 'ronit@text.com' },]
+
+
+
+module.exports=data;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 console.log("hi there")
 console.log('Nodemon is working perfactly')
-
+const data =require('./data')
 const http = require('http')
 http.createServer((req, resp) => {
     resp.writeHead(200, { 'Content-Type': 'applicaton\json' })
-    resp.write(JSON.stringify({ name: 'Suhdeb', email: 'sukhdeb@text.com' }))
+    resp.write(JSON.stringify(data))
     resp.end();
     console.log('test')
+    console.log("Api checked")
 }).listen(5000);
